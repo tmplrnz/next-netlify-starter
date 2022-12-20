afed34e35e70051958fcd269185dfcaa45486c2c
tmplrnz <111866044+tmplrnz@users.noreply.github.com>
2022-12-20 13:14:56 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,47 +11,10 @@ export default function Home() {
       </Head>
 
       <main>
-     
-
-<script type='text/javascript' src='https://service.force.com/embeddedservice/5.0/esw.min.js'></script>
-<script type='text/javascript'>
-	var initESW = function(gslbBaseURL) {
-		embedded_svc.settings.displayHelpButton = true; 
-		embedded_svc.settings.language = ''; 
-
-		
-
-		embedded_svc.settings.enabledFeatures = ['LiveAgent'];
-		embedded_svc.settings.entryFeature = 'LiveAgent';
-
-		embedded_svc.init(
-			'https://crm-comunebergamo--uat.sandbox.my.salesforce.com',
-			'https://crm-comunebergamo--uat.sandbox.my.site.com/',
-			gslbBaseURL,
-			'00D1q0000000O9Z',
-			'Comune_di_Bergamo',
-			{
-				baseLiveAgentContentURL: 'https://c.la1-c1cs-cdg.salesforceliveagent.com/content',
-				deploymentId: '5721q0000008Odc',
-				buttonId: '5731q0000008PtA',
-				baseLiveAgentURL: 'https://d.la1-c1cs-cdg.salesforceliveagent.com/chat',
-				eswLiveAgentDevName: 'Chatbot_Comune_Bergamo',
-				isOfflineSupportEnabled: false
-			}
-		);
-	};
-
-	if (!window.embedded_svc) {
-		var s = document.createElement('script');
-		s.setAttribute('src', 'https://crm-comunebergamo--uat.sandbox.my.salesforce.com/embeddedservice/5.0/esw.min.js');
-		s.onload = function() {
-			initESW(null);
-		};
-		document.body.appendChild(s);
-	} else {
-		initESW('https://service.force.com');
-	}
-</script>
+        <Header title="Welcome to my app!" />
+        <p className="description">
+          Get started by editing <code>pages/index.js</code>
+        </p>
       </main>
 
       <Footer />
